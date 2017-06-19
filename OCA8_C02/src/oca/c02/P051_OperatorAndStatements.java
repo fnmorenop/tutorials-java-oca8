f8d9efa90be7620ca695d4835f06386d3472aca5
fabio.moreno.dlbi <fabio.moreno.dlbi@dev-fnmorenop>
2017-06-19 04:09:23 -0500

    2017-06-19 - Advance till page 51

---
+++ OCA8_C02/src/oca/c02/P051_OperatorAndStatements.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oca.c02;
+
+/**
+ *
+ * @author fabio.moreno.dlbi
+ */
+public class P051_OperatorAndStatements {
+    public static void main(String[] args) {
+        System.out.printf("%n Chapter 2 = %s%n", "Operator and Statements");
+    }
+}
