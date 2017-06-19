7167083d1a6259ee399b0a2fdfde4926a6a3a766
fabio.moreno.dlbi <fabio.moreno.dlbi@dev-fnmorenop>
2017-06-19 04:09:59 -0500

    2017-06-19 - Advance till page 51

---
+++ OCA8_Overall/src/oca8/c01/p042_review_questions/Responses.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oca8.c01.p042_review_questions;
+
+/**
+v 01. A. B. E.
+x 02. A. --> D
+v 03. B. D. E.
+v 04. A. B.
+v 05. C. D.
+v 06. E.
+x 07. A. B. C. D. E. --> A. B. C.
+v 08. B. 
+v 09. A. C. D. E.
+v 10. E.
+v 11. C. D.
+v 12. G.
+v 13. A. D.
+v 14. D.
+v 15. A. E.
+x 16. B. C. -D.
+v 17. A. E.
+x 18. C. -D. E.
+v 19. B. D. 
+x 20. B. D. --> B. E.
+v 21. A.
+v 22. B. E.
+x 23. C. -D.
+
+x 6/23	->	26 %
+v 17/23	->	74 %
+
+ */
+public class Responses {
+    
+}
